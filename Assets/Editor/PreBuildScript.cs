a7d93d4b4ea9e9c0780a5377a1b43a4855a35b28
Brett Kercher <brettkercher@utexas.edu>
2021-11-02 15:05:49 -0500

    move prebuild script to editor folder

--- Assets/Editor/PreBuildScript.cs
+++ Assets/Editor/PreBuildScript.cs
@@ -1,12 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using System.Reflection;
 using Ludiq;
-using UnityEditor;
 using UnityEngine;
 
-public class PreBuildScript : MonoBehaviour
-{
+public class PreBuildScript : MonoBehaviour {
     public static void PreExport() { 
         var init = typeof(PluginContainer).GetMethod("Initialize", BindingFlags.NonPublic | BindingFlags.Static);
         init?.Invoke(null,null); // Force PluginContainer to initialize, since it won't in this context due to a Bolt bug
