f298fa30bfd1893f730cc8fe1e06469c74614ff5
Brett Kercher <brettkercher@utexas.edu>
2021-11-02 14:19:33 -0500

    add bolt

---
+++ Assets/PreBuildScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using Ludiq;
+using UnityEditor;
+using UnityEngine;
+
+public class PreBuildScript : MonoBehaviour
+{
+    public static void PreExport() { 
+        var init = typeof(PluginContainer).GetMethod("Initialize", BindingFlags.NonPublic | BindingFlags.Static);
+        init?.Invoke(null,null); // Force PluginContainer to initialize, since it won't in this context due to a Bolt bug
+        AotPreBuilder.PreCloudBuild(); 
+    }
+}
