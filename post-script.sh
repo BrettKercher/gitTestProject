92095ca59d2053a786a4f6183ab03f9b8f5b9312
Brett Kercher <brettk@unity3d.com>
2022-08-09 14:49:32 -0500

    fail pre build script

--- post-script.sh
+++ post-script.sh
@@ -1,15 +1,4 @@
 echo "======================"
 echo "Hello World from post-build"
-echo "Workspace: $WORKSPACE"
-cd $WORKSPACE
-echo "Contents of Workspace:"
-ls -la
-echo "======================"
-echo "Param 1: $1"
-echo "Param 2: $2"
-echo "Param 3: $3"
-echo "======================"
-echo "Contents of $1:"
-ls -la $1
 echo "======================"
 exit 0
--- pre-script.sh
+++ pre-script.sh
@@ -1,10 +1,4 @@
 echo "======================"
 echo "Hello World from pre-build"
-echo "Param 1: $1"
-echo "Param 2: $2"
-echo "Param 3: $3"
 echo "======================"
-echo "Contents of $1:"
-ls -la $1
-echo "======================"
-exit 0
+exit 1
