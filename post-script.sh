e56753c49794ef7570d12d0bf30291fa4b2366a0
Brett Kercher <brettk@unity3d.com>
2022-09-09 13:04:17 -0500

    asdf

--- post-script.sh
+++ post-script.sh
@@ -1,4 +1,9 @@
 echo "======================"
 echo "Hello World from post-build"
 echo "======================"
+ls
+
+cat build_manifest_old.json
+echo "============"
+cat build_manifest.json
 exit 0
